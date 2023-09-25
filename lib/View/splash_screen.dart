05f05c522ebc27b5451864591c7aebba58ad574f
Aryan Kumar <aryankumawat01@gmail.com>
2023-09-25 23:50:44 +0530

    Added SplashScreen

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math' as math;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,25 +8,46 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin{
-
-
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 3), vsync: this);
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
-
   }
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    print(_controller.value.toString);
     return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-            Text("Hello"),
+          children: [
+            AnimatedBuilder(
+                animation: _controller,
+                child: Container(
+                  height: screenHeight * 0.4,
+                  width: screenWidth * 0.9,
+                  child: const Center(
+                    child: Image(image: AssetImage('images/virus.png')),
+                  ),
+                ),
+                builder: (BuildContext context, Widget? child) {
+                  return Transform.rotate(
+                    angle: _controller.value * 2.0 * math.pi,
+                    child: child,
+                  );
+                }),
+            SizedBox(height: screenHeight*0.08,),
+            const Align(
+                alignment: Alignment.center,
+                child: Text("Covid-19\nTracker App",style: TextStyle(fontWeight: FontWeight.bold,fontSize:25),)),
           ],
         ),
       ),

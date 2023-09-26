a2921a4fd955971c3bf1be136319b5cca1d7019a
Aryan Kumar <aryankumawat01@gmail.com>
2023-09-26 19:06:49 +0530

    Tracker Screen Added

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:flutter/services.dart';
 import 'package:covid_tracker/View/world_states.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
@@ -17,43 +17,49 @@ class _SplashScreenState extends State<SplashScreen>
       AnimationController(duration: Duration(seconds: 3), vsync: this)..repeat();
   @override
   void initState() {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     // TODO: implement initState
     super.initState();
     Timer(const Duration(seconds: 5), () {
-      // Navigator.push(context, MaterialPageRoute(builder: (context) => WorldStatesScreen()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => WorldStatesScreen()));
     });
   }
+
+
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnimatedBuilder(
-                animation: _controller,
-                child: Container(
-                  height: screenHeight * 0.4,
-                  width: screenWidth * 0.9,
-                  child:  Center(
-                    child: Image(image: AssetImage('images/virus.png')),
+      body: AnnotatedRegion(
+        value: Colors.white,
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                  animation: _controller,
+                  child: Container(
+                    height: screenHeight * 0.4,
+                    width: screenWidth * 0.9,
+                    child:  Center(
+                      child: Image(image: AssetImage('images/virus.png')),
+                    ),
                   ),
-                ),
-                builder: (BuildContext context, Widget? child) {
-                  return Transform.rotate(
-                    angle: _controller.value * 2.0 * math.pi,
-                    child: child,
-                  );
-                }),
-            SizedBox(height: screenHeight*0.08,),
-            const Align(
-                alignment: Alignment.center,
-                child: Text("Covid-19\nTracker App",textAlign:TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize:25),)),
-          ],
+                  builder: (BuildContext context, Widget? child) {
+                    return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                      child: child,
+                    );
+                  }),
+              SizedBox(height: screenHeight*0.08,),
+              const Align(
+                  alignment: Alignment.center,
+                  child: Text("Covid-19\nTracker App",textAlign:TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize:25),)),
+            ],
+          ),
         ),
       ),
     );
--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:flutter/services.dart';
 
 class WorldStatesScreen extends StatefulWidget {
   const WorldStatesScreen({super.key});
@@ -7,9 +9,86 @@ class WorldStatesScreen extends StatefulWidget {
   State<WorldStatesScreen> createState() => _WorldStatesScreenState();
 }
 
-class _WorldStatesScreenState extends State<WorldStatesScreen> {
+class _WorldStatesScreenState extends State<WorldStatesScreen> with TickerProviderStateMixin {
+  late final AnimationController _controller =
+  AnimationController(duration: const Duration(seconds: 3), vsync: this)..repeat();
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    super.initState();
+    _controller.dispose();
+  }
+
+  final colorList =<Color> [
+    const Color (0xff4285F4),
+    const Color(0xff1aa260) ,
+    const Color(0xffde5246) ,
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+   final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(height: screenHeight*0.05,),
+            PieChart(
+              animationDuration: const Duration(milliseconds: 1200),
+              chartRadius: screenWidth/3,
+              colorList: colorList,
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left
+                ),
+                chartType: ChartType.ring,
+                dataMap: const {
+              "Total":30,
+              "Recovered":25,
+              "Deaths":5,
+            }),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical:screenHeight*0.06),
+              child: const Card(
+                child: Column(
+                  children: [
+                    ReusableRow(value: "200", title:"Total" ),
+                    ReusableRow(value: "200", title:"Total" ),
+                    ReusableRow(value: "200", title:"Total" ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height:50,
+              decoration:BoxDecoration(
+                color: const Color(0xff1aa260),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Center(child: Text("Track Countries")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableRow extends StatelessWidget {
+  final String title,value;
+  const ReusableRow({super.key,required this.title,required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
+        ],
+      ),
+    );
   }
 }
